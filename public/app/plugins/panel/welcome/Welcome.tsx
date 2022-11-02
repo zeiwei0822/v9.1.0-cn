ed74495b453434e6bd581431298bfb6dd470ef0c
zeiwei0822 <93323801+zeiwei0822@users.noreply.github.com>
2022-11-02 23:29:17 +0800

    Update Welcome.tsx

--- public/app/plugins/panel/welcome/Welcome.tsx
+++ public/app/plugins/panel/welcome/Welcome.tsx
@@ -18,7 +18,7 @@ export const WelcomeBanner: FC = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>歡迎使用Grafana</h1>
       <div className={styles.help}>
-        <h3 className={styles.helpText}>Need help?</h3>
+        <h3 className={styles.helpText}>需要幫忙?</h3>
         <div className={styles.helpLinks}>
           {helpOptions.map((option, index) => {
             return (
