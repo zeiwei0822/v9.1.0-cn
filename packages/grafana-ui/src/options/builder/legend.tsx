7f9dc52d21b1e1e6e86e8e260efaeb9908b10bef
zeiwei0822 <93323801+zeiwei0822@users.noreply.github.com>
2022-11-07 09:19:23 +0800

    Update legend.tsx

--- packages/grafana-ui/src/options/builder/legend.tsx
+++ packages/grafana-ui/src/options/builder/legend.tsx
@@ -11,42 +11,42 @@ export function addLegendOptions<T extends OptionsWithLegend>(
   builder
     .addBooleanSwitch({
       path: 'legend.showLegend',
-      name: 'Visibility',
+      name: '詳細',
       category: ['Legend'],
       description: '',
       defaultValue: true,
     })
     .addRadio({
       path: 'legend.displayMode',
-      name: 'Mode',
+      name: '模式',
       category: ['Legend'],
       description: '',
       defaultValue: LegendDisplayMode.List,
       settings: {
         options: [
-          { value: LegendDisplayMode.List, label: 'List' },
-          { value: LegendDisplayMode.Table, label: 'Table' },
+          { value: LegendDisplayMode.List, label: '列表' },
+          { value: LegendDisplayMode.Table, label: '表格' },
         ],
       },
       showIf: (c) => c.legend.showLegend,
     })
     .addRadio({
       path: 'legend.placement',
-      name: 'Placement',
+      name: '放置於',
       category: ['Legend'],
       description: '',
       defaultValue: 'bottom',
       settings: {
         options: [
-          { value: 'bottom', label: 'Bottom' },
-          { value: 'right', label: 'Right' },
+          { value: 'bottom', label: '底部' },
+          { value: 'right', label: '右側' },
         ],
       },
       showIf: (c) => c.legend.showLegend,
     })
     .addNumberInput({
       path: 'legend.width',
-      name: 'Width',
+      name: '寬度',
       category: ['Legend'],
       settings: {
         placeholder: 'Auto',
@@ -58,9 +58,9 @@ export function addLegendOptions<T extends OptionsWithLegend>(
     builder.addCustomEditor<StatsPickerConfigSettings, string[]>({
       id: 'legend.calcs',
       path: 'legend.calcs',
-      name: 'Values',
+      name: '值',
       category: ['Legend'],
-      description: 'Select values or calculations to show in legend',
+      description: '在圖例中選擇要顯示的值或計算',
       editor: standardEditorsRegistry.get('stats-picker').editor as any,
       defaultValue: [],
       settings: {
