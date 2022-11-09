4ba44d86f68b509f2a5bd8dc0de5d0d669ee9872
zeiwei0822 <93323801+zeiwei0822@users.noreply.github.com>
2022-11-09 22:54:41 +0800

    Update ValueMappingsEditor.tsx

--- public/app/features/dimensions/editors/ValueMappingsEditor/ValueMappingsEditor.tsx
+++ public/app/features/dimensions/editors/ValueMappingsEditor/ValueMappingsEditor.tsx
@@ -93,7 +93,7 @@ export const ValueMappingsEditor = React.memo((props: Props) => {
       </Button>
       <Modal
         isOpen={isEditorOpen}
-        title="Value mappings"
+        title="映射值"
         onDismiss={onCloseEditor}
         className={styles.modal}
         closeOnBackdropClick={false}
