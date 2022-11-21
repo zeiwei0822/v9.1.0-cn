674d94d7b91d127cc92c2f245bc06ac4d2d02fc2
zeiwei0822 <93323801+zeiwei0822@users.noreply.github.com>
2022-11-21 17:03:26 +0800

    Update LiveConnectionWarning.tsx

--- public/app/features/live/LiveConnectionWarning.tsx
+++ public/app/features/live/LiveConnectionWarning.tsx
@@ -52,7 +52,7 @@ export class LiveConnectionWarning extends PureComponent<Props, State> {
 
       return (
         <div className={this.styles.foot}>
-          <Alert severity={'warning'} className={this.styles.warn} title="connection to server is lost..." />
+          <Alert severity={'warning'} className={this.styles.warn} title="與服務器的連接丟失..." />
         </div>
       );
     }
